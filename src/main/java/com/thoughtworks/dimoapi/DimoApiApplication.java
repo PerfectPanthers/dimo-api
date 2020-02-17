1c9266d8c36b63b6700c36219e3410405c746262
Mukesh Ranjan <mukesh.ranjan@thoughtworks.com>
2020-02-17 10:11:12 +0530

    Initial project setup

---
+++ src/main/java/com/thoughtworks/dimoapi/DimoApiApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.dimoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/dimoapi/DimoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.dimoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
