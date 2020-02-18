74f4fbd66695eae489910c0caacf999c2853ea60
ayush.goyal <ayush.goyal@thoughtworks.com>
2020-02-18 10:37:07 +0530

    Resolved Conflicts

--- src/main/java/com/thoughtworks/dimoapi/entity/User.java
+++ src/main/java/com/thoughtworks/dimoapi/entity/User.java
@@ -5,6 +5,10 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 @Document(collection = "users")
 @NoArgsConstructor
 @Data
