059e08f4874725d89eef055370a56286b9edee90
gireeshbhat <gireeshgbhat@gmail.com>
2020-02-19 15:10:07 +0530

    fixed test case

--- src/test/java/com/thoughtworks/dimoapi/controller/UserControllerTest.java
+++ src/test/java/com/thoughtworks/dimoapi/controller/UserControllerTest.java
@@ -5,21 +5,16 @@ import com.thoughtworks.dimoapi.model.LoginRequest;
 import com.thoughtworks.dimoapi.model.Response;
 import com.thoughtworks.dimoapi.service.UserService;
 import com.thoughtworks.dimoapi.utils.PasswordUtils;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
@@ -27,17 +22,8 @@ public class UserControllerTest {
     @Mock
     private UserService userService;
 
-    @Autowired
-    MockMvc mockMVC;
-
     @InjectMocks
     UserController userController;
-
-
-    @Before
-    public void setUp() {
-        mockMVC = MockMvcBuilders.standaloneSetup(userController).build();
-    }
 
     @Test
     public void testDoLoginSuccess() {
@@ -52,7 +38,6 @@ public class UserControllerTest {
     @Test
     public void testDoLoginInvalidUser() {
         User murugesan = new User("1", "Murugesan", "abc@gmail.com");
-
         murugesan.setPassword(PasswordUtils.encrypt("abc"));
         when(userService.findByEmail("abc@gmail.com")).thenReturn(murugesan);
         LoginRequest loginRequest = new LoginRequest("abc@gmail.com", "def");
@@ -79,8 +64,7 @@ public class UserControllerTest {
 
     @Test
     public void testDoLoginUnregisteredUser() {
-
-        when(userService.findByEmail("abc@gmail.com")).thenThrow(new RuntimeException("Could not connect to persistent store"));
+        when(userService.findByEmail("mailnotsaved@gmail.com")).thenReturn(null);
         LoginRequest loginRequest = new LoginRequest("mailnotsaved@gmail.com", "def");
         ResponseEntity<Response> response = userController.login(loginRequest);
         assertEquals(response.getStatusCode(), HttpStatus.BAD_REQUEST);
