b2caddace0e13d997d89f94a441a7d3bb1c9eb48
murugesanp <murugesan148@gmail.com>
2020-02-19 11:12:09 +0530

    Update PropertyUtils.java

--- src/main/java/com/thoughtworks/dimoapi/utils/PropertyUtils.java
+++ src/main/java/com/thoughtworks/dimoapi/utils/PropertyUtils.java
@@ -4,9 +4,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class PropertyUtils {
 
-    protected static Dotenv dotenv = Dotenv.load();
+    //protected static Dotenv dotenv = Dotenv.load();
 
     public static String getProperty(String key) {
-        return dotenv.get(key);
+        //return dotenv.get(key);
+        return "perfectpanthers";
     }
 }
