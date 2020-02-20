9f39c5108f57bd0dab778293a89bc8278fff5a15
gireeshbhat <gireeshgbhat@gmail.com>
2020-02-20 09:53:42 +0530

    <Gireesh> Changed the Response object

--- src/main/java/com/thoughtworks/dimoapi/controller/MovieController.java
+++ src/main/java/com/thoughtworks/dimoapi/controller/MovieController.java
@@ -1,11 +1,14 @@
 package com.thoughtworks.dimoapi.controller;
 
-import com.thoughtworks.dimoapi.model.Response;
 import com.thoughtworks.dimoapi.service.MoviesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/movie")
@@ -14,10 +17,10 @@ public class MovieController {
     @Autowired
     MoviesService moviesService;
 
-    @GetMapping(value = "")
-    public Object getMovieById(@RequestParam(value="id") String movieId) {
-        if(movieId == null || movieId.equals("")){
-            return new ResponseEntity<>(new Response(false,"Empty Movie Id Passed"), HttpStatus.BAD_REQUEST);
+    @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object getMovieById(@RequestParam(value = "id") String movieId) {
+        if (movieId == null || movieId.equals("")) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return moviesService.findBymovieId(Long.parseLong((movieId)));
     }
